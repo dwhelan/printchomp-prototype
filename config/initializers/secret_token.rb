5e8118c87ce2e922dd63a2f7a076ae75d7dcd323
Mike Scriber <mike.scriber@gmail.com>
2014-04-07 20:12:44 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrintchompPrototype::Application.config.secret_token = 'd8f169c190f9708b6ae7e4c41469b36fd650342bdaedbbcebef46633d840ff3529b4888d1c5dd2853ca558e9e4f732e125095bcdbf77bcad71a322c95d407d92'
